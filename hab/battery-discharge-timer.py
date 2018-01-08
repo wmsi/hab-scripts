88b9e54aecc2e3c15a2b942ef08c68fa40adda90
MckennaCisler <mckennacisler@gmail.com>
2018-01-08 10:43:19 -0500

    bat-discharge-time: Fix time bug and add backup file

--- hab/battery-discharge-timer.py
+++ hab/battery-discharge-timer.py
@@ -7,17 +7,22 @@
 # Mckenna Cisler - mckennacisler@gmail.com
 
 import time
+import subprocess
 
 WRITE_FREQ = 5 # secs
 DURATION_FILE = "battery-discharge-time.txt"
+BACKUP_FILE = "battery-discharge-time-backup.txt"
 
 start_time = time.time()
 
 while True:
+    # back up the file in case we have a write issue
+    subprocess.call(["cp", DURATION_FILE, BACKUP_FILE])
+
     with open(DURATION_FILE, "w+") as f:
         elapsed = time.time() - start_time # secs
         out = "%8ds (%5.2fhr) after %s" % (elapsed, \
-            elapsed / 60.0, time.ctime(start_time))
+            elapsed / 3600.0, time.ctime(start_time))
         f.write(out + "\n")
         print(out)
 
